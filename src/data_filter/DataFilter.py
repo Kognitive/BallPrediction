08ddcc2683b122de13f19306dec1e03abbeadd10
markus <m.semmler.94@googlemail.com>
2017-06-28 11:33:55 +0200

    Added DataFilter interface

---
+++ src/data_filter/DataFilter.py
@@ -0,0 +1,32 @@
+# MIT License
+#
+# Copyright (c) 2017 Markus Semmler, Stefan Fabian
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This interface represents a basic DataFilter. It only supports one operation
+# and gets used inside of the data adapter, when the data is loaded.
+
+
+class DataFilter:
+
+    # This method should take the trajectory and return the filtered version of
+    # it.
+    def filter(self, trajectory):
+        raise NotImplementedError("You have to supply a filter method.")
